0df78dd8fbf13a40a3dfcef1eb98f884dfc09c63
Yusuke Ohi <y.ohi@diamondhead.tech>
2022-11-02 19:57:11 +0900

    commit

--- lua/set-docker-compose-env/init.lua
+++ lua/set-docker-compose-env/init.lua
@@ -6,16 +6,17 @@ local function set_docker_compose_env()
     -- print(os.execute('docker compose config --format json | jq ".services.app.environment" > /dev/null'))
         local handle = io.popen('docker compose config --format json | jq ".services.app.environment" &> /dev/null')
         -- local handle = io.popen('docker compose config --format json | jq ".services.app.environment" > /dev/null')
-        -- print(handle)
         if handle ~= nil then
             local result = handle:read("*a")
-            local docker_env = vim.fn.json_decode(result)
-            for key, value in pairs(docker_env) do
-                -- print('-----')
-                -- print(key)
-                -- print(value)
-                vim.fn.setenv(key, value)
-                vim.notify('export ' .. key .. '=' .. value, 'info')
+            if result ~= '' then
+                local docker_env = vim.fn.json_decode(result)
+                for key, value in pairs(docker_env) do
+                    -- print('-----')
+                    -- print(key)
+                    -- print(value)
+                    vim.fn.setenv(key, value)
+                    vim.notify('export ' .. key .. '=' .. value, 'info')
+                end
             end
         end
     -- end
