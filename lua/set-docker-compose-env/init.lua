16361c723d1062d30ce819dc0dfe578a927b17b8
Yusuke Ohi <yohi.consadole12@gmail.com>
2023-04-15 22:07:16 +0900

    ソースをきれいに

--- lua/set-docker-compose-env/init.lua
+++ lua/set-docker-compose-env/init.lua
@@ -2,27 +2,20 @@ vim.notify = require("notify")
 vim.opt.termguicolors = true
 
 local function set_docker_compose_env()
-    -- print('export docker environment')
-    -- print(os.execute('docker compose config --format json | jq ".services.app.environment" > /dev/null'))
-        local notify_list = {}
-        local handle = io.popen('docker compose config --format json | jq ".services.app.environment" &> /dev/null')
-        -- local handle = io.popen('docker compose config --format json | jq ".services.app.environment" > /dev/null')
-        if handle ~= nil then
-            local result = handle:read("*a")
-            if result ~= '' then
-                local docker_env = vim.fn.json_decode(result)
-                for key, value in pairs(docker_env) do
-                    -- print('-----')
-                    -- print(key)
-                    -- print(value)
-                    vim.fn.setenv(key, value)
-                    -- vim.notify('export ' .. key .. '=' .. value, 'info')
-                    table.insert(notify_list, 'export ' .. key .. '=' .. value)
-                end
+    local handle = io.popen('docker compose config --format json | jq ".services.app.environment" &> /dev/null')
+    if handle ~= nil then
+        local result = handle:read("*a")
+        if result ~= '' then
+            -- docker compose config のenvironmentを取得できた場合
+            local docker_env = vim.fn.json_decode(result)
+            local notify_list = {}
+            for key, value in pairs(docker_env) do
+                vim.fn.setenv(key, value)
+                table.insert(notify_list, 'export ' .. key .. '=' .. value)
             end
+            vim.notify(table.concat(notify_list, "\n"), 'info')
         end
-        vim.notify(table.concat(notify_list, "\n"), 'info')
-    -- end
+    end
 end
 
 return {
